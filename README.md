0f159d29b8d41673b3f24d6f0b368082452e770b
Trevor Bai <baijin2011@gmail.com>
2020-05-31 18:49:36 -0400

    modify README files and change favicon

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # React-Apollo-GraphQL
 
-backend: GraphQL, express
-frontend: React, Apollo, Bootstrap
+## Introduction
+This project is a full stack web application where at front end it utilizes React, Apollo, Bootstrap and at back end uses GraphQL and express.<br />
+
+The front end repo is at folder `client` at root directory whereas the back end repo is just at very root level.
+
+## Key Features
+
+1. User gets a whole list of SpaceX launch history and its individual launch details such as what rockets it uses.<br />
+2. Integrates with SpaceX API regarding launches and rockets info.<br />
+3. Utilizes `GraphQL` to retrieve data from SpaceX API.<br />
---
+++ public/build/precache-manifest.4bdcc662030f8b5cf16ba1ab4210f036.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32446dff2d645d96a302b504dddedf71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17134964431b1398b2c9",
+    "url": "/static/css/main.086580c9.chunk.css"
+  },
+  {
+    "revision": "afeadadafd64b03a1517",
+    "url": "/static/js/2.e04fedae.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.e04fedae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17134964431b1398b2c9",
+    "url": "/static/js/main.3a14e143.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "34280cf240c0e786fc55103a2e9c1c10",
+    "url": "/static/media/logo.34280cf2.jpg"
+  }
+]);
